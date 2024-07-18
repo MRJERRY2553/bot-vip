cceb1f2753df4a25c058863d21309b57fb593286
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-18 22:55:24 +0545

    Create 000wsq.js

---
+++ modules/commands/000wsq.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+config: {
+name: "wsq",
+credits: "Vex_Kshitiz",
+version: "2.0",
+cooldowns: 10,
+hasPermission: 0,
+description: "Get random wsq video" 
+commandCategory: "fun",
+usages: "shoti",
+},
+
+run: async function ({ api, event, args, message }) {
+api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+
+try {
+const response = await axios.get("https://whysoquite-93dh.onrender.com/kshitiz");
+const postData = response.data.posts;
+const randomIndex = Math.floor(Math.random() * postData.length);
+const randomPost = postData[randomIndex];
+
+const videoUrls = randomPost.map(url => url.replace(/\\/g, "/"));
+
+const selectedUrl = videoUrls[Math.floor(Math.random() * videoUrls.length)];
+
+const videoResponse = await axios.get(selectedUrl, { responseType: "stream" });
+
+const tempVideoPath = path.join(__dirname, "cache", `${Date.now()}.mp4`);
+const writer = fs.createWriteStream(tempVideoPath);
+videoResponse.data.pipe(writer);
+
+writer.on("finish", async () => {
+const stream = fs.createReadStream(tempVideoPath);
+const user = response.data.user || "@user_unknown";
+await message.reply({
+body: ``,
+attachment: stream,
+});
+api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+fs.unlink(tempVideoPath, (err) => {
+if (err) console.error(err);
+console.log(`Deleted ${tempVideoPath}`);
+});
+});
+} catch (error) {
+console.error(error);
+message.reply("Sorry, an error occurred while processing your request.");
+}
+}
+};
