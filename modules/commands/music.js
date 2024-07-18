5bd9db0c369c1c0a9fdc97e2b25c49d8ebce50c6
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-18 15:09:22 +0545

    Update music.js

--- modules/commands/music.js
+++ modules/commands/music.js
@@ -1,77 +1,112 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const ytdl = require("ytdl-core");
-const yts = require("yt-search");
-
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const { resolve } = require('path');
+async function downloadMusicFromYoutube(link, path) {
+  var timestart = Date.now();
+  if(!link) return 'Missing link'
+  var resolveFunc = function () { };
+  var rejectFunc = function () { };
+  var returnPromise = new Promise(function (resolve, reject) {
+    resolveFunc = resolve;
+    rejectFunc = reject;
+  });
+    ytdl(link, {
+            filter: format =>
+                format.quality == 'tiny' && format.audioBitrate == 48 && format.hasAudio == true
+        }).pipe(fs.createWriteStream(path))
+        .on("close", async () => {
+            var data = await ytdl.getInfo(link)
+            var result = {
+                title: data.videoDetails.title,
+                dur: Number(data.videoDetails.lengthSeconds),
+                viewCount: data.videoDetails.viewCount,
+                likes: data.videoDetails.likes,
+                author: data.videoDetails.author.name,
+                timestart: timestart
+            }
+            resolveFunc(result)
+        })
+  return returnPromise
+}
 module.exports.config = {
     name: "music",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "Jonell Magallanes",
-    description: "Play a music",
-    commandCategory: "Utility",
-    usages: "music (search)",
-    usePrefix: true,
-    cooldowns: 5,
+    credits: "RAHUL",
+    description: "à¦®à¦¿à¦‰à¦œà¦¿à¦•ðŸ°",
+    commandCategory: "utilities",
+    usages: "[searchMusic]",
+    cooldowns: 0
 };
 
-module.exports.run = async function ({ api, event }) {
-    const input = event.body;
-    const text = input.substring(12);
-    const data = input.split(" ");
-
-    if (data.length < 2) {
-        return api.sendMessage("Please put a song", event.threadID);
-    }
-
-    data.shift();
-    const song = data.join(" ");
-
+module.exports.handleReply = async function ({ api, event, handleReply }) {
+    const axios = require('axios')
+    const { createReadStream, unlinkSync, statSync } = require("fs-extra")
     try {
-        api.sendMessage(`Finding "${song}". Please wait...`, event.threadID);
+        var path = `${__dirname}/cache/1.mp3`
+        var data = await downloadMusicFromYoutube('https://www.youtube.com/watch?v=' + handleReply.link[event.body -1], path);
+        if (fs.statSync(path).size > 26214400) return api.sendMessage('The file cannot be sent because the capacity is greater than 25MB.', event.threadID, () => fs.unlinkSync(path), event.messageID);
+        api.unsendMessage(handleReply.messageID)
+        return api.sendMessage({ 
+    body: `ðŸŽµ Title: ${data.title}\nðŸŽ¶ Name Channel : ${data.author}\nâ±ï¸ Time: ${this.convertHMS(data.dur)}\nðŸ‘€ Views: ${data.viewCount}\nðŸ¥° Likes: ${data.likes}\nâ±ï¸Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\nâ™ª====ã€Œ ðŠð¡ðšð§ ð‘ðšð¡ð®ð¥ ð‘ðŠ ã€====â™ª`,
+            attachment: fs.createReadStream(path)}, event.threadID, ()=> fs.unlinkSync(path), 
+         event.messageID)
 
-        const searchResults = await yts(song);
-
-        if (!searchResults || !searchResults.videos || searchResults.videos.length === 0) {
-            return api.sendMessage("Error: No videos found.", event.threadID, event.messageID);
-        }
-
-        const video = searchResults.videos[0];
-        const videoUrl = video.url;
-
-        const stream = ytdl(videoUrl, { filter: "audioonly" });
-
-        const fileName = `${event.senderID}.mp3`;
-        const filePath = __dirname + `/cache/${fileName}`;
-
-        stream.pipe(fs.createWriteStream(filePath));
-
-        stream.on('response', () => {
-            console.info('[DOWNLOADER]', 'Starting download now!');
-        });
-
-        stream.on('info', (info) => {
-            console.info('[DOWNLOADER]', `Downloading ${info.videoDetails.title} by ${info.videoDetails.author.name}`);
-        });
-
-        stream.on('end', () => {
-            console.info('[DOWNLOADER] Downloaded');
-
-            if (fs.statSync(filePath).size > 26214400) {
-                fs.unlinkSync(filePath);
-                return api.sendMessage('[ERR] The file could not be sent because it is larger than 25MB.', event.threadID);
-            }
-
-            const message = {
-                body: `Here's your music, enjoy!🥰\n\nTitle: ${video.title}\nArtist: ${video.author.name}`,
-                attachment: fs.createReadStream(filePath)
-            };
-            api.sendMessage(message, event.threadID, () => {
-                fs.unlinkSync(filePath);
-            });
-        });
-    } catch (error) {
-        console.error('[ERROR]', error);
-        api.sendMessage('An error occurred while processing the command.', event.threadID);
     }
-};
+    catch (e) { return console.log(e) }
+}
+module.exports.convertHMS = function(value) {
+    const sec = parseInt(value, 10); 
+    let hours   = Math.floor(sec / 3600);
+    let minutes = Math.floor((sec - (hours * 3600)) / 60); 
+    let seconds = sec - (hours * 3600) - (minutes * 60); 
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return (hours != '00' ? hours +':': '') + minutes+':'+seconds;
+}
+module.exports.run = async function ({ api, event, args }) {
+    if (args.length == 0 || !args) return api.sendMessage('Â» à¦‰à¦«à¦« à¦†à¦¬à¦¾à¦² à¦•à¦¿ à¦—à¦¾à¦¨ à¦¶à§à¦¨à¦¤à§‡ à¦šà¦¾à¦¸ à¦¤à¦¾à¦° à§¨/à§§ à¦²à¦¾à¦‡à¦¨ à¦¤à§‹ à¦²à§‡à¦–à¦¬à¦¿ à¦¨à¦¾à¦•à¦¿ ðŸ¥µ empty!', event.threadID, event.messageID);
+    const keywordSearch = args.join(" ");
+    var path = `${__dirname}/cache/1.mp3`
+    if (fs.existsSync(path)) { 
+        fs.unlinkSync(path)
+    }
+    if (args.join(" ").indexOf("https://") == 0) {
+        try {
+            var data = await downloadMusicFromYoutube(args.join(" "), path);
+            if (fs.statSync(path).size > 26214400) return api.sendMessage('Unable to send files because the capacity is greater than 25MB .', event.threadID, () => fs.unlinkSync(path), event.messageID);
+            return api.sendMessage({ 
+                body: `ðŸŽµ Title: ${data.title}\nðŸŽ¶ Name Channel: ${data.author}\nâ±ï¸ Time: ${this.convertHMS(data.dur)}\nðŸ‘€ Views: ${data.viewCount}\nðŸ‘ Likes: ${data.likes}\nâ±ï¸ Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\nðŸ’¿====ðŠð¡ðšð§ ð‘ðšð¡ð®ð¥ ð‘ðŠ====ðŸ’¿`,
+                attachment: fs.createReadStream(path)}, event.threadID, ()=> fs.unlinkSync(path), 
+            event.messageID)
+
+        }
+        catch (e) { return console.log(e) }
+    } else {
+          try {
+            var link = [],
+                msg = "",
+                num = 0
+            const Youtube = require('youtube-search-api');
+            var data = (await Youtube.GetListByKeyword(keywordSearch, false,6)).items;
+            for (let value of data) {
+              link.push(value.id);
+              num = num+=1
+              msg += (`${num} - ${value.title} (${value.length.simpleText})\n\n`);
+            }
+            var body = `Â»ðŸ”Ž There's ${link.length} the result coincides with your search keyword:\n\n${msg}Â» Reply(feedback) select one of the searches above `
+            return api.sendMessage({
+              body: body
+            }, event.threadID, (error, info) => global.client.handleReply.push({
+              type: 'reply',
+              name: this.config.name,
+              messageID: info.messageID,
+              author: event.senderID,
+              link
+            }), event.messageID);
+          } catch(e) {
+            return api.sendMessage('An error has occurred, please try again in a moment!!\n' + e, event.threadID, event.messageID);
+        }
+    }
+                                                                                                                                                                                                       }
