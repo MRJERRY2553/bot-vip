6fef953d638a0e2fcc4402fa2407a42b6b61cdf0
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-20 11:17:44 +0545

    Update music.js

--- modules/commands/music.js
+++ modules/commands/music.js
@@ -1,147 +1,77 @@
-// Fixed by Mohammad Nayan. Dont Change Credit
+const axios = require("axios");
+const fs = require("fs-extra");
+const ytdl = require("ytdl-core");
+const yts = require("yt-search");
 
+module.exports.config = {
+    name: "music",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes",
+    description: "Play a music",
+    commandCategory: "Utility",
+    usages: "music (search)",
+    usePrefix: false,
+    cooldowns: 30,
+};
 
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const { resolve } = require('path');
-const axios = require("axios")
-async function downloadMusicFromYoutube(link, path) {
-  if (!link) return 'Link Not Found';
+module.exports.run = async function ({ api, event }) {
+    const input = event.body;
+    const text = input.substring(12);
+    const data = input.split(" ");
 
-  const timestart = Date.now();
+    if (data.length < 2) {
+        return api.sendMessage("Please put a song", event.threadID);
+    }
 
-  try {
-    const audioUrl = data.data.audio;
+    data.shift();
+    const song = data.join(" ");
 
-    return new Promise((resolve, reject) => {
-      axios({
-        method: 'get',
-        url: audioUrl,
-        responseType: 'stream'
-      }).then(response => {
-        const writeStream = fs.createWriteStream(path);
-
-        response.data.pipe(writeStream)
-          .on('finish', async () => {
-            try {
-              const info = await ytdl.getInfo(link);
-              const result = {
-                title: info.videoDetails.title,
-                dur: Number(info.videoDetails.lengthSeconds),
-                viewCount: info.videoDetails.viewCount,
-                likes: info.videoDetails.likes,
-                author: info.videoDetails.author.name,
-                timestart: timestart
-              };
-              resolve(result);
-            } catch (error) {
-              reject(error);
-            }
-          })
-          .on('error', (error) => {
-            reject(error);
-          });
-      }).catch(error => {
-        reject(error);
-      });
-    });
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
-
-
-
-module.exports = {
-  config: {
-    
-  name: "music", 
-  version: "1.0.0", 
-  permission: 0,
-  credits: "Nayan",
-  description: "example",
-  prefix: true,
-  category: "Media", 
-  usages: "user", 
-  cooldowns: 5,
-  dependencies: {
-    "axios":"",
-    "fs":"",
-    	  "ytdl-core":"",
-    "simple-youtube-api":""
-	}
-},
-
-handleReply: async function ({ api, event, handleReply }) {
-    const axios = require('axios')
-    const { createReadStream, unlinkSync, statSync } = require("fs-extra")
     try {
-        var path = `${__dirname}/cache/1.mp3`
-        var data = await downloadMusicFromYoutube('https://www.youtube.com/watch?v=' + handleReply.link[event.body -1], path);
-        if (fs.statSync(path).size > 26214400) return api.sendMessage('The file cannot be sent because the capacity is greater than 25MB.', event.threadID, () => fs.unlinkSync(path), event.messageID);
-        api.unsendMessage(handleReply.messageID)
-        return api.sendMessage({ 
-		body: `ðŸŽµ Title: ${data.title}\nðŸŽ¶ Name Channel : ${data.author}\nâ±ï¸ Time: ${this.convertHMS(data.dur)}\nðŸ‘€ Views: ${data.viewCount}\nðŸ¥° Likes: ${data.likes}\nâ±ï¸Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\nðŸ’¿====DISME PROJECT====ðŸ’¿`,
-            attachment: fs.createReadStream(path)}, event.threadID, ()=> fs.unlinkSync(path), 
-         event.messageID)
-            
-    }
-    catch (e) { return console.log(e) }
-},
-  
-convertHMS: function(value) {
-    const sec = parseInt(value, 10); 
-    let hours   = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - (hours * 3600)) / 60); 
-    let seconds = sec - (hours * 3600) - (minutes * 60); 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return (hours != '00' ? hours +':': '') + minutes+':'+seconds;
-},
-  
-  start: async function ({ nayan, events, args }) {
-    if (args.length == 0 || !args) return nayan.reply('Â» à¦‰à¦«à¦« à¦†à¦¬à¦¾à¦² à¦•à¦¿ à¦—à¦¾à¦¨ à¦¶à§à¦¨à¦¤à§‡ à¦šà¦¾à¦¸ à¦¤à¦¾à¦° à§¨/à§§ à¦²à¦¾à¦‡à¦¨ à¦¤à§‹ à¦²à§‡à¦–à¦¬à¦¿ à¦¨à¦¾à¦•à¦¿ ðŸ¥µ empty!', events.threadID, events.messageID);
-    const keywordSearch = args.join(" ");
-    var path = `${__dirname}/cache/1.mp3`
-    if (fs.existsSync(path)) { 
-        fs.unlinkSync(path)
-    }
-    if (args.join(" ").indexOf("https://") == 0) {
-        try {
-            var data = await downloadMusicFromYoutube(args.join(" "), path);
-            if (fs.statSync(path).size > 26214400) return nayan.reply('Unable to send files because the capacity is greater than 25MB .', events.threadID, () => fs.unlinkSync(path), events.messageID);
-            return nayan.reply({ 
-                body: `ðŸŽµ Title: ${data.title}\nðŸŽ¶ Name Channel: ${data.author}\nâ±ï¸ Time: ${this.convertHMS(data.dur)}\nðŸ‘€ Views: ${data.viewCount}\nðŸ‘ Likes: ${data.likes}\nâ±ï¸ Processing time: ${Math.floor((Date.now()- data.timestart)/1000)} second\nðŸ’¿====DISME PROJECT====ðŸ’¿`,
-                attachment: fs.createReadStream(path)}, events.threadID, ()=> fs.unlinkSync(path), 
-            events.messageID)
+        api.sendMessage(`Finding "${song}". Please wait...`, event.threadID);
 
+        const searchResults = await yts(song);
+
+        if (!searchResults || !searchResults.videos || searchResults.videos.length === 0) {
+            return api.sendMessage("Error: No videos found.", event.threadID, event.messageID);
         }
-        catch (e) { return console.log(e) }
-    } else {
-          try {
-            var link = [],
-                msg = "",
-                num = 0
-            const Youtube = require('youtube-search-api');
-            var data = (await Youtube.GetListByKeyword(keywordSearch, false,6)).items;
-            for (let value of data) {
-              link.push(value.id);
-              num = num+=1
-              msg += (`${num} - ${value.title} (${value.length.simpleText})\n\n`);
+
+        const video = searchResults.videos[0];
+        const videoUrl = video.url;
+
+        const stream = ytdl(videoUrl, { filter: "audioonly" });
+
+        const fileName = `${event.senderID}.mp3`;
+        const filePath = __dirname + `/cache/${fileName}`;
+
+        stream.pipe(fs.createWriteStream(filePath));
+
+        stream.on('response', () => {
+            console.info('[DOWNLOADER]', 'Starting download now!');
+        });
+
+        stream.on('info', (info) => {
+            console.info('[DOWNLOADER]', `Downloading ${info.videoDetails.title} by ${info.videoDetails.author.name}`);
+        });
+
+        stream.on('end', () => {
+            console.info('[DOWNLOADER] Downloaded');
+
+            if (fs.statSync(filePath).size > 26214400) {
+                fs.unlinkSync(filePath);
+                return api.sendMessage('[ERR] The file could not be sent because it is larger than 25MB.', event.threadID);
             }
-            var body = `Â»ðŸ”Ž There's ${link.length} the result coincides with your search keyword:\n\n${msg}Â» Reply(feedback) select one of the searches above `
-            return nayan.reply({
-              body: body
-            }, events.threadID, (error, info) => global.client.handleReply.push({
-              type: 'reply',
-              name: this.config.name,
-              messageID: info.messageID,
-              author: events.senderID,
-              link
-            }), events.messageID);
-          } catch(e) {
-            return nayan.reply('An error has occurred, please try again in a moment!!\n' + e, events.threadID, events.messageID);
-        }
+
+            const message = {
+                body: `Here's your music, enjoy!🥰\n\nTitle: ${video.title}\nArtist: ${video.author.name}`,
+                attachment: fs.createReadStream(filePath)
+            };
+            api.sendMessage(message, event.threadID, () => {
+                fs.unlinkSync(filePath);
+            });
+        });
+    } catch (error) {
+        console.error('[ERROR]', error);
+        api.sendMessage('An error occurred while processing the command.', event.threadID);
     }
-                                                                                                                                                                                                       }}
+};
